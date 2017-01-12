833d4c428eae162c397837a4b2423a60ff49221a
liangchengcheng <1038127753@qq.com>
2017-01-13 07:33:53 +0800

    add about page utils
    add about page utils

---
+++ src/main/java/com/lcc/core/orm/PageRequest.java
@@ -0,0 +1,100 @@
+package com.lcc.core.orm;
+
+/**
+ * Created by lcc on 2017/1/13.
+ */
+public class PageRequest {
+
+    protected int pageNo = 1;
+
+    protected int pageSize = 10;
+
+    protected String orderBy = "id";
+
+    protected String orderDir = Sort.DESC;
+
+    protected int offset = 1;
+
+    protected int totalCount = -1;
+
+    protected int totalPage;
+
+    public PageRequest(){}
+
+    public PageRequest(int pageNo, int pageSize){
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+        this.offset = (pageNo -1) * pageSize;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+
+        if(pageNo < 1){
+            this.pageNo = 1;
+        }
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+
+        if(pageSize < 1){
+            this.pageSize = 1;
+        }
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public String getOrderDir() {
+        return orderDir;
+    }
+
+    public void setOrderDir(String orderDir) {
+        this.orderDir = orderDir;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public static class Sort{
+        public static final String ASC = "asc";
+
+        public static final String DESC = "desc";
+
+    }
+}
---
+++ src/main/java/com/lcc/core/orm/Pagination.java
@@ -0,0 +1,60 @@
+package com.lcc.core.orm;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by lcc on 2017/1/13.
+ */
+public class Pagination<T> extends PageRequest implements Iterable<T>,Serializable {
+    protected List<T> items = null;
+
+    public Pagination(){}
+
+    public Pagination(PageRequest pageRequest){
+        this.pageNo = pageRequest.getPageNo();
+        this.pageSize = pageRequest.getPageSize();
+        this.orderBy = pageRequest.getOrderBy();
+        this.orderDir = pageRequest.getOrderDir();
+        this.offset = pageRequest.getOffset();
+        this.totalCount = pageRequest.getTotalCount();
+        this.totalPage = pageRequest.getTotalPage();
+    }
+
+    public Iterator<T> iterator() {
+        return items.iterator();
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+    public void setItems(List<T> items) {
+        this.items = items;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+}
