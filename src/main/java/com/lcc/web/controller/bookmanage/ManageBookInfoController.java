825ecf125b2a920157fe59b5aad1068def85ceb2
liangchengcheng <1038127753@qq.com>
2017-01-15 20:22:17 +0800

    add controller class file
    add controller class file

---
+++ src/main/java/com/lcc/web/controller/bookmanage/ManageBookInfoController.java
@@ -0,0 +1,65 @@
+package com.lcc.web.controller.bookmanage;
+
+import com.lcc.domain.BookInfo;
+import com.lcc.domain.BookTable;
+import com.lcc.service.book.ManageBookInfoService;
+import com.lcc.service.book.ManageBookTableService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "manage/book")
+public class ManageBookInfoController {
+    @Resource(name = "manageBookInfoService")
+    private ManageBookInfoService manageBookInfoService;
+
+    @Resource(name = "manageBookTableService")
+    private ManageBookTableService manageBookTableService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String index(HttpServletRequest request,Model model){
+        List<BookInfo> bookInfoList = manageBookInfoService.findList();
+        List<BookTable> bookTableList = manageBookTableService.findAll();
+
+        model.addAttribute("bookInfoList", bookInfoList);
+        model.addAttribute("bookTableList", bookTableList);
+        return "manage/book/list";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String editPage(Model model) {
+
+        model.addAttribute("type", 1);
+        return "manage/book/edit";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editPage(@PathVariable Integer id, Model model) {
+        BookInfo bookInfo = manageBookInfoService.findById(id);
+
+        model.addAttribute("bookInfo", bookInfo);
+        model.addAttribute("type", 2);
+        return "manage/book/edit";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String insert(@ModelAttribute BookInfoFormBean bookInfoFormBean) {
+        manageBookInfoService.insert(bookInfoFormBean);
+
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(@ModelAttribute BookInfoFormBean bookInfoFormBean) {
+        manageBookInfoService.update(bookInfoFormBean);
+        return "redirect:list";
+    }
+
+}
---
+++ src/main/java/com/lcc/web/controller/categorymanage/ManageCategoryController.java
@@ -0,0 +1,47 @@
+package com.lcc.web.controller.categorymanage;
+
+import com.google.common.collect.Maps;
+import com.lcc.domain.CategoryInfo;
+import com.lcc.service.category.ManageCategoryService;
+import com.lcc.service.category.SimpleCategoryInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "manage/category")
+public class ManageCategoryController {
+
+    @Resource(name = "manageCategoryService")
+    private ManageCategoryService manageCategoryService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    private String list(HttpServletRequest request, Model model) {
+        List<SimpleCategoryInfo> simpleCategoryInfoList = manageCategoryService.list();
+
+        model.addAttribute("simpleCategoryList", simpleCategoryInfoList);
+        return "manage/category/list";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    private String insert(@ModelAttribute CategoryInfo categoryInfo, HttpServletRequest request) {
+        manageCategoryService.insert(categoryInfo);
+
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    private String update(@ModelAttribute CategoryInfo categoryInfo, HttpServletRequest request) {
+        manageCategoryService.updateBySlug(categoryInfo);
+
+        return "redirect:list";
+    }
+}
