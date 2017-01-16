d1fdadb7d9c8de19396f1c7773bdeb7fc2666d39
liangchengcheng <1038127753@qq.com>
2017-01-16 08:32:35 +0800

    add config xml file
    add config xml file

---
+++ src/main/java/com/lcc/web/controller/ResumeController.java
@@ -0,0 +1,17 @@
+package com.lcc.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ResumeController {
+
+    @RequestMapping(value = "/resume", method = RequestMethod.GET)
+    public String index(HttpServletRequest request) {
+
+        return "web/resume/resume";
+    }
+}
